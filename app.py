da04e328aae42f7906dda0d6ff7c5de39c8dfdf3
James Dindi <dindijjames@gmail.com>
2018-01-25 22:52:07 +0300

    Finalize on registration

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from data import Articles
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -110,7 +111,19 @@ def login():
     
     return render_template('login.html')
 
+# Check if user is logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorised! Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
     return render_template('dashboard.html')
 
